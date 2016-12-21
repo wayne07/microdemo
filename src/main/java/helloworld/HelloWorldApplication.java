383ab11319b1dadb56ab9b7ed49f4cd64c5eb79a
Juergen Seliger <juergen.seliger@gmail.com>
2016-12-21 19:21:02 +0100

    formatierung

--- src/main/java/helloworld/HelloWorldApplication.java
+++ src/main/java/helloworld/HelloWorldApplication.java
@@ -24,10 +24,7 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
     @Override
     public void run(HelloWorldConfiguration configuration,
                     Environment environment) {
-        final HelloWorldResource resource = new HelloWorldResource(
-                configuration.getTemplate(),
-                configuration.getDefaultName()
-        );
+        final HelloWorldResource resource = new HelloWorldResource(configuration.getTemplate(), configuration.getDefaultName());
         environment.jersey().register(resource);
     }
 
